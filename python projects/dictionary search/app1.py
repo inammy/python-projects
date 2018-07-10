c9f9fbd502e0ff8ed81bcd4a2fe6e9b32167606c
Nishant <nishant.soni@edgenetworks.in>
2018-07-10 12:04:04 +0530

    added 'dictionary search' project

---
+++ python projects/dictionary search/app1.py
@@ -0,0 +1,21 @@
+import json
+from difflib import get_close_matches
+data = json.load(open("data.json"))
+
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean '%s' instead? Enter Y for yes, N for no: " % get_close_matches(w,data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(w,data.keys())[0]]
+        elif yn == "N":
+            return "The word you entered doesn't exist."
+        else:
+            return "We didn't understand your input."
+    else:
+        return "The word doesn't exist, please correct it."
+
+word = input("Enter your word: ")
+print(translate(word))
